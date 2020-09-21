a77b8afa8de94eb7670f947fc496a3a1928338ef
christian@christian-steinert.de <christian@christian-steinert.de>
2020-09-21 02:16:19 +0200

    Version 0.0.15

--- buildAndroid.sh
+++ buildAndroid.sh
@@ -1,3 +1,7 @@
 #!/bin/dash
 export ANDROID_HOME=/home/christian/Android/Sdk
 cd _build/; ./buildAndroid.sh; cd ..
+
+
+APP_VERSION=`cat _build/mobile/tibetandict/config.xml |grep '<widget' | sed  's#.* version="\([^"]*\)".*#\1#'`
+cp TibetanDictionary-PUBLIC.apk "old releases/${APP_VERSION} (`date +"%B %Y"`, Android 4.4+).apk"
