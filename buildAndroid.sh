08b28c6ffe42d1a53fc29c7e7f01abdb8e5165e3
christian@christian-steinert.de <christian@christian-steinert.de>
2021-01-21 21:17:05 +0100

    Fix backspace behavior for Android 10 and 11

--- buildAndroid.sh
+++ buildAndroid.sh
@@ -4,4 +4,5 @@ cd _build/; ./buildAndroid.sh; cd ..
 
 
 APP_VERSION=`cat _build/mobile/tibetandict/config.xml |grep '<widget' | sed  's#.* version="\([^"]*\)".*#\1#'`
+LANG=en_us_88591
 cp TibetanDictionary-PUBLIC.apk "old releases/${APP_VERSION} (`date +"%B %Y"`, Android 4.4+).apk"
