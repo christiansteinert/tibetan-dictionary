0c5ec43311cd54f79cbb747163967044e05a59b1
christian@christian-steinert.de <christian@christian-steinert.de>
2024-11-01 09:19:33 +0100

    fix typo in Negi

--- _input/dictionaries/conversion/Negi/negi-convert.py
+++ _input/dictionaries/conversion/Negi/negi-convert.py
@@ -10,7 +10,7 @@ from devatrans import DevaTrans
 
 def prepareLinebreaks(skt):
     skt = re.sub(r'(i|ii|iii|iv|v|vi|vii|viii|ix|x|xi|xii|xiii|xiv|xv|xvi)\.','\n\\1.',skt) 
-    skt = re.sub('•','\\n\\n•',skt) 
+    skt = re.sub(r'•','\\n\\n•',skt) 
     return skt
 
 def fixRomanNumerals(skt):
@@ -19,24 +19,24 @@ def fixRomanNumerals(skt):
 
 def hideUnwantedTib(tib): # suppress Tibetan entries with characters that we cannot properly handle
   if tib:
-    tib = re.sub('.*[…\.,/•|].*','',tib) 
-    tib = re.sub('^[^()]*\([^()]*','',tib)  # Remove words with brackets that only open 
-    tib = re.sub('^[^()]*\)[^()]*','',tib)  # Remove words with brackets that only close
+    tib = re.sub(r'.*[…\.,/•|].*','',tib) 
+    tib = re.sub(r'^[^()]*\([^()]*','',tib)  # Remove words with brackets that only open 
+    tib = re.sub(r'^[^()]*\)[^()]*','',tib)  # Remove words with brackets that only close
     return tib
   else:
     return ''
 
 def cleanTib(tib): # clean garbage from Tibetan 
   tib = tib.strip()
-  tib = tib.replace('°','')
-  tib = tib.replace('?','')
-  tib = tib.replace('~','')
-  tib = tib.replace('ˇ','')
-  tib = tib.replace('-','')
+  tib = tib.replace(r'°','')
+  tib = tib.replace(r'?','')
+  tib = tib.replace(r'~','')
+  tib = tib.replace(r'ˇ','')
+  tib = tib.replace(r'-','')
   tib = tib.replace('+\'a','A')
-  tib = tib.replace('*','')
+  tib = tib.replace(r'*','')
   tib = tib.replace('\n',' ')
-  tib = re.sub('\s+',' ', tib)
+  tib = re.sub(r'\s+',' ', tib)
   
   return tib
 
@@ -47,17 +47,17 @@ def cleanDef(skt): # clean garbage from Sanskrit
   return skt
 
 def wrapWylie(skt): #wrap wylie-transliterated Tibetan text
-  skt = re.sub("([+a-zA-Z']+[+a-zA-Z' ]*[+a-zA-Z'/]+)",'{\\1}',skt)
+  skt = re.sub(r"([+a-zA-Z']+[+a-zA-Z' ]*[+a-zA-Z'/]+)",'{\\1}',skt)
   return skt
 
 def cleanSkt(skt): # clean garbage from Sanskrit
   if skt:
     skt = skt.strip()
     skt = skt.replace('\n','\\n')
-    skt = skt.replace('|','\\n')
-    skt = skt.replace('।','\\n')
-    skt = skt.replace('ॐ','OM')
-    skt = re.sub('\s+',' ', skt)
+    skt = skt.replace(r'|','\\n')
+    skt = skt.replace(r'।','\\n')
+    skt = skt.replace(r'ॐ','OM')
+    skt = re.sub(r'\s+',' ', skt)
 
     skt = re.sub('^\\n','',skt) 
     skt = re.sub('^\\n','',skt) 
@@ -93,7 +93,7 @@ for item in root.findall('item'):
     for tib2 in tib.split('='):
       for tib3 in tib2.split(','):
         if '(' in tib3:
-          tibWithoutBracketContent = re.sub('\([^)]+\)','',tib3)
+          tibWithoutBracketContent = re.sub(r'\([^)]+\)','',tib3)
           f.write("%s|%s\n" % ( tibWithoutBracketContent.strip(), sktTrans ) )
 #          tibWithoutBrackets = re.sub('[()]','',tib3)
 #          f.write("%s|%s\n" % ( cleanTib(tibWithoutBrackets), sktTrans ) )
